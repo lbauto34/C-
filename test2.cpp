0ff2ce0e7df47f65c900db0442fd2bd8d5beffb3
lbauto34 <liubiaoauto@163.com>
2019-08-25 14:50:36 +0800

    update 20190825

---
+++ test2.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+
+using namespace std;
+
+int t;
+
+int main()
+{
+    cin >> t;
+    while(t--)
+    {
+        cout << "hello" << endl;
+
+    }
+    system("pause");
+    return 0;
+}
